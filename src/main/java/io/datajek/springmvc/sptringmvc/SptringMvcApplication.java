a8c68c934fd272f8d102fce3b86d9ad7a34cb933
ITIvanov18 <ITIvanov18@codingburgas.bg>
2022-11-30 15:28:18 +0200

    Spring tags of MVC forms

---
+++ src/main/java/io/datajek/springmvc/sptringmvc/SptringMvcApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.springmvc.sptringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SptringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/datajek/springmvc/sptringmvc/SptringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.springmvc.sptringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
